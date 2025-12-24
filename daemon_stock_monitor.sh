29c932eb10a05b2e9fb5be857bc7fff923a5e512
Maro2058 <moradelshorbagy2005@gmail.com>
2025-12-24 15:34:13 +0200

    Added Option for daemon stock monitor to remove items from low stock items list after they were restocked

--- daemon_stock_monitor.sh
+++ daemon_stock_monitor.sh
@@ -3,14 +3,17 @@
 # Daemon script to monitor stock quantity and send alerts
 LOG_FILE="./system_logs.txt"
 LOW_STOCK_ITEMS="./low_stock_items.txt"
+cutoff=5
 
-# 1. Safety Check
+# 1. Safety Checks
+# Make sure inventory file exists
 if [ ! -f ./inventory.txt ]
 then
     echo "Inventory file not found."
     exit 1
 fi
 
+# Create Low Stock items file if it doesn't exist
 if [ ! -f "$LOW_STOCK_ITEMS" ]
 then
     touch "$LOW_STOCK_ITEMS"
@@ -21,27 +24,35 @@ echo "Stock Daemon Started..." >> "$LOG_FILE"
 # 2. The Infinite Daemon Loop
 while true
 do
+    tempfile=$(mktemp)
+
     # Read the inventory file line by line
     # We feed the file into the loop at the very bottom using '<'
     while IFS=':' read -r id name quantity
     do
+    now=$(date "+%H:%M:%S")
         # Skip empty lines to prevent errors
         if [ -z "$id" ]; then continue; fi
 
         # 3. The Condition (Check if quantity < 5)
-        if [ "$quantity" -lt 5 ]; then
-            
+        if [ "$quantity" -lt $cutoff ]; then
             if ! grep -q "^$id:$name:" "$LOW_STOCK_ITEMS"; then
-                
-                now=$(date "+%H:%M:%S")
-                
                 # Log only because it's a NEW alert
-                echo "[$now] ALERT: Low stock for '$name'! Only $quantity left." >> "$LOG_FILE"
+                echo "[$now] ALERT: Low stock for '$name' | $quantity left." >> "$LOG_FILE"
                 echo "$id:$name:$quantity" >> "$LOW_STOCK_ITEMS" 
+            fi
+        # Check if quantity > 5)
+        elif [ "$quantity" -ge $cutoff ]; then
+            # If item is in Low stock items
+            if  grep -q "^$id:$name" "$LOW_STOCK_ITEMS" ; then
+                echo "[$now] INFO: $name restocked" >> "$LOG_FILE"
+                grep -v "^$id:$name" "$LOW_STOCK_ITEMS" > "$tempfile" && mv "$tempfile" "$LOW_STOCK_ITEMS"
             fi
         fi
     done < ./inventory.txt  # <--- This feeds the file into the inner loop
 
     # Sleep for 5 seconds before checking again
     sleep 5
+
+    rm -f "$tempfile"
 done
